07023e1d32c790c943c47194bdb82c495c9a83b2
Gary Tou <gary@garytou.com>
2021-03-09 22:09:49 -0800

    admin login tweaks

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,154 +1,173 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { CircularProgress, Typography } from "@material-ui/core";
-import PropTypes from "prop-types";
+import { Typography } from "@material-ui/core";
 import styles from "../../styles/admin/Admin.module.css";
 import { Close } from "@material-ui/icons";
 
 import game from "../../helpers/game";
+import consts from "../../config/consts";
 
 import Link from "next/link";
 import {
-  Box,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  Container,
-  IconButton,
-  Paper,
-  Snackbar,
-  TextField,
+	Button,
+	Card,
+	CardContent,
+	Container,
+	IconButton,
+	Snackbar,
+	TextField,
 } from "@material-ui/core";
 
-import { signInWithGoogle } from "../../utils/firebase";
 import firebase from "../../utils/firebase";
 
 export default function Admin() {
-  const [inputText, setInputText] = useState("");
-  const router = useRouter();
+	const [inputText, setInputText] = useState("");
+	const router = useRouter();
 
-  const [openSnackBar, setOpenSnackBar] = useState(false);
-  const [barMessage, setMessage] = useState("");
+	const [openSnackBar, setOpenSnackBar] = useState(false);
+	const [barMessage, setBarMessage] = useState("");
 
-  useEffect(async () => {
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user && user.email !== null) {
-        if (user.email.endsWith("@wafbla.org")) {
-          console.log("already logged in, redirecting to dashboard");
-          router.replace(`/admin/dashboard`);
-        }
-      }
-    });
-  }, []);
+	useEffect(() => {
+		const unsub = firebase.auth().onAuthStateChanged(function (user) {
+			// if logged in
+			if (user && user.email !== null) {
+				// if email is eligible to be an admin
+				if (game.validAdminEmail(user.email)) {
+					console.log("already logged in, redirecting to dashboard");
+					router.replace(`/admin/dashboard`);
+				}
+			}
+		});
 
-  const handleCloseSnackBar = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setOpenSnackBar(false);
-  };
+		return unsub;
+	}, []);
 
-  async function passwordCheck() {
-    if (await game.checkPd(inputText)) {
-      googleSignInPopup();
-    }
-  }
+	const handleCloseSnackBar = (event, reason) => {
+		if (reason === "clickaway") {
+			return;
+		}
+		setOpenSnackBar(false);
+	};
 
-  function googleSignInPopup() {
-    var provider = new firebase.auth.GoogleAuthProvider();
+	async function passwordCheck() {
+		try {
+			if (await game.checkAdminPassword(inputText)) {
+				googleSignInPopup();
+				setBarMessage(
+					`Welcome, please select your ${consts.adminEmailDomain} email.`
+				);
+				setOpenSnackBar(true);
+				setInputText("");
+			}
+		} catch (error) {
+			setBarMessage(error.message);
+			setOpenSnackBar(true);
+		}
+	}
 
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then((result) => {
-        /** @type {firebase.auth.OAuthCredential} */
-        var credential = result.credential;
-        var user = result.user;
+	function googleSignInPopup() {
+		const provider = new firebase.auth.GoogleAuthProvider();
 
-        if (user.email.endsWith("@wafbla.org")) {
-          console.log("wafbla account, all good");
-          //redirect to dashboard
-          router.replace(`/admin/dashboard`);
-        } else {
-          console.log("not a wafbla account");
-          user
-            .delete()
-            .then(function () {
-              //display message to user that sign in operation/account is deleted
-              setOpenSnackBar(true);
-              setMessage("Admins must sign in through a WAFBLA account");
-            })
-            .catch(function (error) {
-              // An error happened.
-            });
-        }
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        var email = error.email;
-        var credential = error.credential;
-      });
-  }
+		firebase
+			.auth()
+			.signInWithPopup(provider)
+			.then((result) => {
+				const credential = result.credential;
+				const user = result.user;
 
-  return (
-    <>
-      <div style={{ padding: "10px" }}>
-        <Link href="/">
-          <Button id={styles.adminButton}>Back to home</Button>
-        </Link>
-        <Container id={styles.homeContainer} style={{ minHeight: "60vh" }}>
-          <Typography variant="h2" id={styles.title} gutterBottom>
-            Admin
-          </Typography>
-          <Card className={styles.gamePinCard}>
-            <CardContent className={styles.pinContainer}>
-              <TextField
-                placeholder="ADMIN PASSWORD"
-                type={"password"}
-                onChange={(event) => {
-                  setInputText(event.target.value);
-                }}
-                id={styles.pinInput}
-                inputProps={{ style: { textAlign: "center" } }}
-              />
+				if (game.validAdminEmail(user.email)) {
+					console.log("admin account, all good");
+					//redirect to dashboard
+					router.replace(`/admin/dashboard`);
+				} else {
+					console.log("not an admin account");
+					user
+						.delete()
+						.then(function () {
+							//display message to user that sign in operation/account is deleted
+							setOpenSnackBar(true);
+							setBarMessage(
+								"Admins must sign in through their organization account"
+							);
+						})
+						.catch(function (error) {
+							console.error(error);
+							// An error happened.
+						});
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				var email = error.email;
+				var credential = error.credential;
+			});
+	}
 
-              <Button
-                variant="contained"
-                onClick={() => passwordCheck()}
-                color="primary"
-                id={styles.adminButton}
-              >
-                Sign in!
-              </Button>
-            </CardContent>
-          </Card>
-          {openSnackBar && (
-            <Snackbar
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              open={openSnackBar}
-              autoHideDuration={6000}
-              onClose={handleCloseSnackBar}
-              message={barMessage}
-              action={
-                <IconButton
-                  size="small"
-                  aria-label="close"
-                  color="inherit"
-                  onClick={handleCloseSnackBar}
-                >
-                  <Close fontSize="small" />
-                </IconButton>
-              }
-            />
-          )}
-        </Container>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div style={{ padding: "10px" }}>
+				<Link href='/'>
+					<Button variant='text'>Back to home</Button>
+				</Link>
+				<Container id={styles.homeContainer} style={{ minHeight: "60vh" }}>
+					<Typography variant='h2' id={styles.title} gutterBottom>
+						Admin Dashboard
+					</Typography>
+					<Card className={styles.gamePinCard}>
+						<CardContent className={styles.pinContainer}>
+							<TextField
+								placeholder='ADMIN PASSWORD'
+								type={"password"}
+								onChange={(event) => {
+									setInputText(event.target.value);
+								}}
+								onKeyDown={(e) => {
+									if (e.key === "Enter") {
+										passwordCheck();
+										e.preventDefault();
+									}
+								}}
+								value={inputText}
+								id={styles.pinInput}
+								inputProps={{ style: { textAlign: "center" } }}
+							/>
+
+							<Button
+								variant='contained'
+								onClick={() => passwordCheck()}
+								color='primary'
+								id={styles.adminButton}
+							>
+								Sign in
+							</Button>
+						</CardContent>
+					</Card>
+					{openSnackBar && (
+						<Snackbar
+							anchorOrigin={{
+								vertical: "bottom",
+								horizontal: "left",
+							}}
+							open={openSnackBar}
+							autoHideDuration={6000}
+							onClose={handleCloseSnackBar}
+							message={barMessage}
+							action={
+								<IconButton
+									size='small'
+									aria-label='close'
+									color='inherit'
+									onClick={handleCloseSnackBar}
+								>
+									<Close fontSize='small' />
+								</IconButton>
+							}
+						/>
+					)}
+				</Container>
+			</div>
+		</>
+	);
 }
